3f8a6a043b4e6e7d69e43493b2d8868800736457
Nipaaaa1 <Frostone224@gmail.com>
2024-05-15 16:31:57 +0700

    feat: add filter options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuTrigger,
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
 import { ChevronDownIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
@@ -13,6 +13,11 @@ import { ChangeEvent, useEffect, useState } from "react";
 import axios from "axios";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
+import {
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from "@radix-ui/react-dropdown-menu";
 
 interface Country {
   name: {
@@ -29,6 +34,7 @@ interface Country {
 export default function Home() {
   const [data, setData] = useState<Country[]>();
   const [search, setSearch] = useState<string>();
+  const [region, setRegion] = useState<string>("asia");
 
   let searchTimeout: any;
 
@@ -50,8 +56,9 @@ export default function Home() {
 
   useEffect(() => {
     if (search) {
+      console.log(search);
       getData(
-        `https://restcountries.com/v3.1/name/${search}?fields=flags,name,population,region,capital`,
+        `https://restcountries.com/v3.1/name/${search.toLowerCase()}?fields=flags,name,population,region,capital`,
       );
     }
     console.log(data);
@@ -79,45 +86,59 @@ export default function Home() {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-48 p-4">
-          <DropdownMenuItem>Africa</DropdownMenuItem>
-          <DropdownMenuItem>America</DropdownMenuItem>
-          <DropdownMenuItem>Asia</DropdownMenuItem>
-          <DropdownMenuItem>Europe</DropdownMenuItem>
-          <DropdownMenuItem>Oceania</DropdownMenuItem>
+          <DropdownMenuLabel>
+            <span className="font-semibold">Region:</span> {region}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={region} onValueChange={setRegion}>
+            <DropdownMenuRadioItem value="Africa">Africa</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="America">
+              America
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="Asia">Asia</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="Europe">Europe</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="Oceania">
+              Oceania
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
         </DropdownMenuContent>
       </DropdownMenu>
       <main className="grid w-full grid-cols-1 gap-6">
         {data
-          ? data.map((country) => (
-              <Card key={country.name.common} className="overflow-clip">
-                <CardHeader className="p-0">
-                  <Image
-                    src={country.flags.svg}
-                    alt={`${country.name.common}'s flag`}
-                    width={0}
-                    height={0}
-                    className="h-max w-full border-b"
-                  />
-                </CardHeader>
-                <CardContent className="space-y-4 p-6">
-                  <span className="font-extrabold">{country.name.common}</span>
-                  <ul>
-                    <li>
-                      <span className="font-semibold">Population:</span>{" "}
-                      {country.population}
-                    </li>
-                    <li>
-                      <span className="font-semibold">Region:</span>{" "}
-                      {country.region}
-                    </li>
-                    <li>
-                      <span className="font-semibold">Capital:</span>{" "}
-                      {country.capital}
-                    </li>
-                  </ul>
-                </CardContent>
-              </Card>
-            ))
+          ? data
+              .filter((data) => data.region == region)
+              .map((country) => (
+                <Card key={country.name.common} className="overflow-clip">
+                  <CardHeader className="p-0">
+                    <Image
+                      src={country.flags.svg}
+                      alt={`${country.name.common}'s flag`}
+                      width={0}
+                      height={0}
+                      className="h-max w-full border-b"
+                    />
+                  </CardHeader>
+                  <CardContent className="space-y-4 p-6">
+                    <span className="font-extrabold">
+                      {country.name.common}
+                    </span>
+                    <ul>
+                      <li>
+                        <span className="font-semibold">Population:</span>{" "}
+                        {country.population}
+                      </li>
+                      <li>
+                        <span className="font-semibold">Region:</span>{" "}
+                        {country.region}
+                      </li>
+                      <li>
+                        <span className="font-semibold">Capital:</span>{" "}
+                        {country.capital}
+                      </li>
+                    </ul>
+                  </CardContent>
+                </Card>
+              ))
           : ((<p>Start searching</p>) as any)}
       </main>
     </>
