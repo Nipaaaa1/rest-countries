f68043f54afaf5ff36c30f3d91488efd1aa4945a
Nipaaaa1 <Frostone224@gmail.com>
2024-05-17 15:37:44 +0700

    feat: add light theme toggle

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,18 +2,28 @@
 
 import { useTheme } from "next-themes";
 import { Button } from "./ui/button";
-import { MoonIcon } from "@radix-ui/react-icons";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 export const ThemeToggle = () => {
   const { setTheme } = useTheme();
   return (
-    <Button
-      variant={"ghost"}
-      className="flex items-center gap-1 bg-white font-semibold text-foreground dark:bg-card"
-      onClick={() => setTheme("dark")}
-    >
-      <MoonIcon className="size-5" />
-      <span>Dark Mode</span>
-    </Button>
+    <>
+      <Button
+        variant={"ghost"}
+        className="flex items-center gap-1 bg-white font-semibold text-foreground dark:hidden"
+        onClick={() => setTheme("dark")}
+      >
+        <MoonIcon className="size-5" />
+        <span>Dark Mode</span>
+      </Button>
+      <Button
+        variant={"ghost"}
+        className="hidden items-center gap-1 bg-card font-semibold text-foreground dark:flex"
+        onClick={() => setTheme("light")}
+      >
+        <SunIcon className="size-5" />
+        <span>Light Mode</span>
+      </Button>
+    </>
   );
 };
